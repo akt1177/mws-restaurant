1394a80a2fc574bfce109ad2533d50289abc69d6
Angela Tam <akt1177@gmail.com>
2018-07-29 11:31:50 -0400

    Update to Leaflet API for maps

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,9 +1,43 @@
 let restaurant;
-var map;
+var newMap;
 
 /**
  * Initialize Google map, called from HTML.
  */
+ document.addEventListener('DOMContentLoaded', (event) => {
+   initMap();
+ });
+
+ /**
+  * Initialize leaflet map
+  */
+ initMap = () => {
+   fetchRestaurantFromURL((error, restaurant) => {
+     if (error) { // Got an error!
+       console.error(error);
+     } else {
+       self.newMap = L.map('map', {
+         center: [restaurant.latlng.lat, restaurant.latlng.lng],
+         zoom: 16,
+         scrollWheelZoom: false
+       });
+       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+         mapboxToken: 'pk.eyJ1IjoiYW5nZWl6YWhveSIsImEiOiJjamphM29rZWEzYTJwM2tvNG84MmR6enRzIn0.F2u-rxXUBM7cW04WcFWIWw',
+         maxZoom: 18,
+         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+         id: 'mapbox.streets'
+       }).addTo(newMap);
+       fillBreadcrumb();
+       DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
+     }
+   });
+ }
+
+
+
+/* old google maps
 window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
@@ -18,7 +52,7 @@ window.initMap = () => {
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
     }
   });
-}
+}*/
 
 /**
  * Get current restaurant from page URL.
